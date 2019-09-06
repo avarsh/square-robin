d9bd6c8dc554aeb13f2b8dd4673817f5dd21043b
Aporva Varshney <varshney.aporva@gmail.com>
2019-09-06 23:57:06 +0100

    Sidebar switches views

--- src/frontend/renderer.js
+++ src/frontend/renderer.js
@@ -1,6 +1,7 @@
 const {ipcRenderer} = require('electron');
 const Handlebars = require('handlebars');
 window.$ = window.jQuery = require('./resources/jquery-3.4.1.min.js');
+const today = new Date();
 
 function onNameInput() {
     // Send the name to the backend
@@ -13,6 +14,23 @@ function onNameInput() {
     loadDefaultView();
 }
 
+function switchViews(view) {
+    if (view == 'daily') {
+        $('#tasks-view').hide(200);
+        $('#daily-view').show(200);
+    } else if (view == 'tasks') {
+        $('#daily-view').hide(200);
+        $('#tasks-view').show(200);
+    }
+}
+
+function hideAllDaily() {
+    $('#daily-tasks-container').hide();
+    $('#no-tasks-container-daily').hide();
+    $('#none-generated-container').hide();
+    $('#all-done-container').hide();
+}
+
 function loadDefaultView() {
     let data = ipcRenderer.sendSync('user-data-request');
 
@@ -22,7 +40,29 @@ function loadDefaultView() {
     let greetingTemplate = Handlebars.compile(greetingTemplateScript);
     $('#greeting').html(greetingTemplate({'username' : data['username']}));
 
-    $('#default-view').show(100);
+    if (data['tasks'].length == 0) {
+        switchViews('tasks');
+        $('#no-tasks-container-all').show();
+        hideAllDaily();
+        $('#no-tasks-container-daily').show();
+    } else {
+        switchViews('daily');
+        if (data['daily'].length == 0) {
+            if (data['timestamp'] == today.toDateStr()) {
+                // We must have completed all the tasks for today
+                hideAllDaily();
+                $('#all-done-container').show();
+            } else {
+                // We need to generate some - TODO
+            }
+        } else {
+            // Fill daily tasks template - TODO
+        }
+
+        // Fill all tasks template - TODO
+    }
+
+    $('#default-view').show(200);
 }
 
 $(window).on('load', () => {
@@ -42,6 +82,8 @@ $(window).on('load', () => {
 
     // Set up callbacks
     $("#name-submit").click(onNameInput);
+    $('#daily-btn').click(() => switchViews('daily'));
+    $('#tasks-btn').click(() => switchViews('tasks'));
 
     // We try to get the user data
     let userData = ipcRenderer.sendSync('user-data-request', null);
